c0f04b43da05823fc0536194b731f4261575593b
Jake Samuels <samuels242@googlemail.com>
2025-08-20 23:11:09 +0100

    Hero section text content

--- components/home/hero/HeroSection.jsx
+++ components/home/hero/HeroSection.jsx
@@ -2,12 +2,23 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="section__hero">
-      <h1>Nexus Accounting</h1>
+    <section className="hero">
+      <div className="hero__text-content">
+        <h1>Nexus Accounting</h1>
 
-      <h2>Expert Accounting Solutions for Small Business</h2>
+        <h2>
+          <span>Expert Accounting Solutions </span> for Small Business
+        </h2>
 
-      <Link href="/about-us">Learn More</Link>
+        <p>
+          Welcome to <strong>Nexus Accounting</strong>. We go beyond the numbers
+          to provide expert accounting and strategic financial solutions for
+          your business. Our mission is to handle your financial complexities so
+          you can focus on what you do best: growing your company.
+        </p>
+
+        <Link href="/about-us">Learn More</Link>
+      </div>
     </section>
   );
 };
